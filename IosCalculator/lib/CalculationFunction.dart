cede93d0a7d69a66aed6e9ac898cd4e96f4313e2
Vincent <vincent@macbookpro.dmz.al-h-konzern.de>
2025-10-28 10:24:59 +0100

    Test Änderung

--- IosCalculator/lib/CalculationFunction.dart
+++ IosCalculator/lib/CalculationFunction.dart
@@ -1,14 +1,13 @@
 String calculationFunction(String input){
   final String americanInput = input.replaceAll(',', '.');
-  final regex = RegExp(r'\d+(?:.\d+)?|[-+×÷%]\gm');
-  final matches= regex.firstMatch(americanInput);
-  //List<String> americanInputList = matches.map((m) => m.group(0)!).toList();
-/*
-  String result = '0';
+  final regex = RegExp(r'\d+(?:\.\d+)?|[-+×÷%]\gm');
+  final Iterable<RegExpMatch> matches= regex.allMatches(americanInput);
+  List<String> americanInputList = matches.map((m) => m.group(0)!).toList();
+
+  String result = '00';
 
   for(String element in americanInputList){
     result += '$element ';
   }
-  */
-  return 'matches';
+  return result;
 }
